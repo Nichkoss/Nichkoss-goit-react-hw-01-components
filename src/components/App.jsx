e7161016185546e27e6f6c4655dac7d1da2cc0fa
Nichkoss <lanchuvvv@gmail.com>
2024-02-26 00:45:53 +0100

    Task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+import { Container } from './App.styled';
+import { Profile } from './Profile/Profile';
+import user from './Profile/user.json'; 
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      ></Profile>
+    </Container>
   );
 };
---
+++ src/components/App.styled.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 20px;
+  padding-right: 20px;
+  padding-top: 20px`
